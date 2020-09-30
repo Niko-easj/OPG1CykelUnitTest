6d01a3570ed5e4fe4b92c95e0f1f63cbfea5d44b
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2020-09-30 20:58:43 +0200

    Add project files.

---
+++ CykelUnitTest/Cykel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CykelUnitTest
+{
+    public class Cykel
+    {
+        private int _id; 
+        private string _farve;
+        private double _pris;
+        private int _gear;
+
+        public Cykel()
+        {
+                
+        }
+
+        public Cykel(int id, string farve, double pris, int gear)
+        {
+            _id = id;
+            _farve = farve;
+            _pris = pris;
+            _gear = gear;
+        }
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        // Farve skal mindst være et tegn lang
+        public string Farve
+        {
+            get => _farve;
+            set
+            {
+                // Hvis farven ikke har et tegn, smid exception.
+                if (value.Length < 1) throw new ArgumentOutOfRangeException();
+                _farve = value;
+            }
+        }
+
+        // Prisen skal være > 0
+        public double Pris
+        {
+            get => _pris;
+            set
+            {
+                // Hvis prisen ikke er over 0 kroner, smid en exception. Ingen gratis cykler.
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                _pris = value;
+            }
+        }
+
+        // Gear på cyklen skal gå fra "3 <= gear <= 32"
+        public int Gear
+        {
+            get => _gear;
+            set
+            {
+                // Hvis cyklen har mellem 3 & 32 gear, bliver det accepteret. Alt under eller over, smid exception.
+                if (value < 3 || value > 32) throw new ArgumentOutOfRangeException();
+                _gear = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Farve)}: {Farve}, {nameof(Pris)}: {Pris}, {nameof(Gear)}: {Gear}";
+        }
+    }
+}
---
+++ CykelUnitTestTests/CykelTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CykelUnitTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CykelUnitTest.Tests
+{
+    [TestClass()]
+    public class CykelTests
+    {
+        private Cykel _cykel;
+
+        //Køre før alle testmetoder.
+        //Opretter et cykelobject, så der er noget at arbejde med.
+        [TestInitialize]
+        public void Init()
+        {
+            _cykel = new Cykel(1, "gul",1500,5);
+        }
+
+        //Vi tester lige konstruktøren, om værdierne er det samme.
+        [TestMethod()]
+        public void CykelTestConstructor()
+        {
+            Assert.AreEqual(1, _cykel.Id);
+            Assert.AreEqual("gul", _cykel.Farve);
+            Assert.AreEqual(1500, _cykel.Pris);
+            Assert.AreEqual(5, _cykel.Gear);
+        }
+
+        //Hvad hvis farven er tom, hvis ja, smid exception.
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CykelTestFarveLength()
+        {
+            //Der skal angives tegn, er stringen tom, smid exception.
+            //Vi forvendter at string er tom, så den skal fejle (grøn)
+            _cykel.Farve = "";
+        }
+
+        //Hvis cyklens pris er 0kr (gratis), smid exception.
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void CykelTestPris()
+        {
+            //
+            _cykel.Pris = 0;
+        }
+
+        //Cyklen skal havde mellem 3 og 32 gear.
+        [TestMethod()]
+        public void CykelTestGear()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _cykel.Gear = 34);
+        }
+    }
+}
